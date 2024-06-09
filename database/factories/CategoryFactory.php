5de9533e5e8baa246d361bd67d4485cdca251263
sourav majumder <sourav.cse18.duet@gmail.com>
2024-06-09 17:57:02 +0600

    create fake data to category & subcategory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'slug' => $this->faker->slug,
+            'status' => random_int(0, 1),
+            'order_by' => random_int(1, 25)
+        ];
+    }
+}
---
+++ database/factories/SubCategoryFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SubCategory>
+ */
+class SubCategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'slug' => $this->faker->slug,
+            'category_id' => random_int(45, 69),
+            'status' => random_int(0, 1),
+            'order_by' => random_int(1, 50)
+        ];
+    }
+}
