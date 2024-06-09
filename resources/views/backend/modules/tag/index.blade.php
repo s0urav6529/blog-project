f22456e562fee342d0d9b6ca6bcab51b868141aa
sourav majumder <sourav.cse18.duet@gmail.com>
2024-06-09 17:20:45 +0600

    No tag found message during no tag found in table

--- resources/views/backend/modules/tag/index.blade.php
+++ resources/views/backend/modules/tag/index.blade.php
@@ -31,44 +31,55 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach ($tag_data as $index => $tag)
+                            @if ($tag_data->isEmpty())
                                 <tr>
-                                    <td>{{ $index + 1 }}</td>
-                                    <td>{{ $tag->name }}</td>
-                                    <td>{{ $tag->slug }}</td>
-                                    <td>{{ $tag->order_by }}</td>
-                                    <td class="{{ $tag->status == 1 ? 'text-success' : 'text-danger' }}">
-                                        {{ $tag->status == 1 ? 'Active' : 'Inactive' }}
-                                    </td>
-                                    <td>{{ $tag->created_at->toDateTimeString() }}</td>
-                                    <td>{{ $tag->created_at != $tag->updated_at ? $tag->updated_at->toDateTimeString() : 'Not updated' }}
-                                    </td>
-                                    <td>
-                                        <div class="d-flex justify-content-center">
-                                            <a href="{{ route('tag.show', $tag->id) }}"><button
-                                                    class="btn btn-info btn-sm"><i class="fa-solid fa-eye"></i></button></a>
-
-                                            <a href="{{ route('tag.edit', $tag->id) }}"><button
-                                                    class="btn btn-warning btn-sm mx-1"><i
-                                                        class="fa-solid fa-edit"></i></button></a>
-
-                                            {!! Form::open([
-                                                'method' => 'delete',
-                                                'id' => 'form_' . $tag->id,
-                                                'route' => ['tag.destroy', $tag->id],
-                                            ]) !!}
-
-                                            {!! Form::button('<i class="fa-solid fa-trash"></i>', [
-                                                'type' => 'button',
-                                                'data-id' => $tag->id,
-                                                'class' => 'delete btn btn-danger btn-sm',
-                                            ]) !!}
-
-                                            {!! Form::close() !!}
+                                    <td colspan="8" class="text-center">
+                                        <div class="alert alert-warning mb-0" role="alert">
+                                            <strong>No Tag Found !</strong>
                                         </div>
                                     </td>
                                 </tr>
-                            @endforeach
+                            @else
+                                @foreach ($tag_data as $index => $tag)
+                                    <tr>
+                                        <td>{{ $index + 1 }}</td>
+                                        <td>{{ $tag->name }}</td>
+                                        <td>{{ $tag->slug }}</td>
+                                        <td>{{ $tag->order_by }}</td>
+                                        <td class="{{ $tag->status == 1 ? 'text-success' : 'text-danger' }}">
+                                            {{ $tag->status == 1 ? 'Active' : 'Inactive' }}
+                                        </td>
+                                        <td>{{ $tag->created_at->toDateTimeString() }}</td>
+                                        <td>{{ $tag->created_at != $tag->updated_at ? $tag->updated_at->toDateTimeString() : 'Not updated' }}
+                                        </td>
+                                        <td>
+                                            <div class="d-flex justify-content-center">
+                                                <a href="{{ route('tag.show', $tag->id) }}"><button
+                                                        class="btn btn-info btn-sm"><i
+                                                            class="fa-solid fa-eye"></i></button></a>
+
+                                                <a href="{{ route('tag.edit', $tag->id) }}"><button
+                                                        class="btn btn-warning btn-sm mx-1"><i
+                                                            class="fa-solid fa-edit"></i></button></a>
+
+                                                {!! Form::open([
+                                                    'method' => 'delete',
+                                                    'id' => 'form_' . $tag->id,
+                                                    'route' => ['tag.destroy', $tag->id],
+                                                ]) !!}
+
+                                                {!! Form::button('<i class="fa-solid fa-trash"></i>', [
+                                                    'type' => 'button',
+                                                    'data-id' => $tag->id,
+                                                    'class' => 'delete btn btn-danger btn-sm',
+                                                ]) !!}
+
+                                                {!! Form::close() !!}
+                                            </div>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            @endif
                         </tbody>
                     </table>
                     {{-- pagination open --}}
