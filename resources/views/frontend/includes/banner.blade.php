a33fb5f846ff936d6dd4ac36af9a820461ba4f7a
sourav majumder <sourav.cse18.duet@gmail.com>
2024-07-06 17:32:53 +0600

    localization minor update

--- resources/views/frontend/includes/banner.blade.php
+++ resources/views/frontend/includes/banner.blade.php
@@ -7,18 +7,9 @@
                     <img src="{{ $post->photo }}" alt="">
                     <div class="item-content">
                         <div class="main-content">
-                            <div class="meta-category">
-                                <span>{{ $post->category?->name }} | </span>
-                                <span>{{ $post->sub_category?->name }}</span>
-                            </div>
                             <a href="{{ route('front.single', $post->slug) }}">
                                 <h4>{{ Str::substr($post->title, 0, 30) . '...' }}</h4>
                             </a>
-                            <ul class="post-info">
-                                <li><a href="#">{{ $post->user?->name }}</a></li>
-                                <li><a href="#">{{ $post->created_at->format('M d, Y') }}</a></li>
-                                <li><a href="#">12 Comments</a></li>
-                            </ul>
                         </div>
                     </div>
                 </div>
--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -3,16 +3,16 @@
         <div class="row">
             <div class="col-lg-12">
                 <ul class="social-icons">
-                    <li><a href="#">Facebook</a></li>
-                    <li><a href="#">Twitter</a></li>
-                    <li><a href="#">Behance</a></li>
-                    <li><a href="#">Linkedin</a></li>
-                    <li><a href="#">Dribbble</a></li>
+                    <li><a href="#">{{ __('Facebook') }}</a></li>
+                    <li><a href="#">{{ __('Twitter') }}</a></li>
+                    <li><a href="#">{{ __('Threads') }}</a></li>
+                    <li><a href="#">{{ __('Linkedin') }}</a></li>
+                    <li><a href="#">{{ __('Instagram') }}</a></li>
                 </ul>
             </div>
             <div class="col-lg-12">
                 <div class="copyright-text">
-                    <p>Copyright &copy; {{ date('Y')}} @SM Blog
+                    <p>Copyright &copy; {{ date('Y') }} @SM Blog
 
                         | Design & Devloped By: <a rel="nofollow" href="https://s0urav6529.github.io/sourav-portfolio/"
                             target="_parent">Sourav Majumder</a></p>
